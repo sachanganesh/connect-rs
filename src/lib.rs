5073c967a0f47fad5d16b4fbadeca0f59bcd4d11
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2021-02-04 11:38:17 -0800

    explain protobuf choice and bump version to 0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
-//! This crate provides a reliable, fault-tolerant, and brokerless message-queue abstraction over
-//! asynchronous network streams.
+//! This crate provides a simple brokerless message-queue abstraction over asynchronous network
+//! streams.
 //!
 //! # Why?
 //! When building networked applications, developers shouldn't have to focus on repeatedly solving
 //! the problem of reliable, fault-tolerant message delivery over byte-streams. By using a message
 //! queue abstraction, crate users can focus on core application logic and leave the low-level
 //! networking and message-queue guarantees to the abstraction.
+//!
+//! # Examples
+//! Please use the [examples](https://github.com/sachanganesh/connect-rs/tree/main/examples)
+//! provided to help understand crate usage.
 //!
 //! # Protobuf
 //! This crate relies on the use of [Protocol Buffers](https://developers.google.com/protocol-buffers)
@@ -14,9 +18,11 @@
 //! decide what Protobuf message type it is, and correspondingly unpack the `Any` into a particular
 //! message type.
 //!
-//! # Examples
-//! Please use the [examples](https://github.com/sachanganesh/connect-rs/tree/main/examples)
-//! provided to help understand crate usage.
+//! Protobuf was chosen when the library hit a roadblock with Rust's `TypeId` potentially not being
+//! consistent between Rust compiler versions. The crate requires a consistent way to determine what
+//! type of message is received, so it can appropriately deserialize the message from network bytes.
+//! Until the Rust ecosystem around reflection improves, the crate will use Protobuf to fill the
+//! void.
 
 mod reader;
 pub(crate) mod schema;
